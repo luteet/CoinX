63a7df27c7830e3ced692cd5c46b90edaa21c6ab
Roman Lukjanets <64837849+luteet@users.noreply.github.com>
2022-03-29 17:46:06 +0300

    v1.0

--- js/main.js
+++ js/main.js
@@ -1,106 +1,123 @@
 
 const body = document.querySelector('body'),
-    html = document.querySelector('html'),
-    menu = document.querySelectorAll('._burger, .header__nav, body'),
-    burger = document.querySelector('._burger'),
-    header = document.querySelector('.header');
+  html = document.querySelector('html'),
+  menu = document.querySelectorAll('._burger, .header__nav, body'),
+  burger = document.querySelector('._burger'),
+  header = document.querySelector('.header');
 
-/* NiceSelect.bind(document.getElementById("example"),{
-  searchable: false
-}); */
+
+const select = document.querySelectorAll('._select');
+
+select.forEach(thisElement => {
+  NiceSelect.bind(thisElement, {
+    searchable: false
+  });
+})
 
 let thisTarget;
 body.addEventListener('click', function (event) {
 
-    thisTarget = event.target;
+  thisTarget = event.target;
 
-    // Меню в шапке
-    if (thisTarget.closest('._burger')) {
-        menu.forEach(elem => {
-            elem.classList.toggle('_active')
-        })
+  // Меню в шапке
+  if (thisTarget.closest('._burger')) {
+    menu.forEach(elem => {
+      elem.classList.toggle('_active')
+    })
+  }
+
+  let themeLink = thisTarget.closest('._theme-btn');
+  if (themeLink) {
+    event.preventDefault();
+
+    if (themeLink.classList.contains('_light-theme')) {
+
+      themeLink.classList.remove('_light-theme');
+      themeLink.classList.remove('_icon-dark-theme');
+      themeLink.classList.add('_dark-theme');
+      themeLink.classList.add('_icon-light-theme');
+
+      localStorage.setItem('theme', 'dark');
+      body.classList.add('_dark-theme');
+
+      /* let themeIcon = themeLink.querySelector('._icon-theme-to-dark');
+      themeIcon.classList.remove('_icon-theme-to-dark');
+      themeIcon.classList.add('_icon-theme-to-light'); */
+
+    } else if (themeLink.classList.contains('_dark-theme')) {
+
+      themeLink.classList.remove('_dark-theme');
+      themeLink.classList.remove('_icon-light-theme');
+      themeLink.classList.add('_light-theme');
+      themeLink.classList.add('_icon-dark-theme');
+
+      localStorage.setItem('theme', 'light');
+      body.classList.remove('_dark-theme');
+
+      /* let themeIcon = themeLink.querySelector('._icon-theme-to-light');
+      themeIcon.classList.remove('_icon-theme-to-light');
+      themeIcon.classList.add('_icon-theme-to-dark'); */
+
     }
-
-    let themeLink = thisTarget.closest('._theme-btn');
-    if(themeLink) {
-      event.preventDefault();
-
-      if(themeLink.classList.contains('_light-theme')) {
-
-        themeLink.classList.remove('_light-theme');
-        themeLink.classList.remove('_icon-dark-theme');
-        themeLink.classList.add('_dark-theme');
-        themeLink.classList.add('_icon-light-theme');
-
-        localStorage.setItem('theme', 'dark');
-        body.classList.add('_dark-theme');
-
-        /* let themeIcon = themeLink.querySelector('._icon-theme-to-dark');
-        themeIcon.classList.remove('_icon-theme-to-dark');
-        themeIcon.classList.add('_icon-theme-to-light'); */
-        
-      } else if(themeLink.classList.contains('_dark-theme')) {
-
-        themeLink.classList.remove('_dark-theme');
-        themeLink.classList.remove('_icon-light-theme');
-        themeLink.classList.add('_light-theme');
-        themeLink.classList.add('_icon-dark-theme');
-
-        localStorage.setItem('theme', 'light');
-        body.classList.remove('_dark-theme');
-
-        /* let themeIcon = themeLink.querySelector('._icon-theme-to-light');
-        themeIcon.classList.remove('_icon-theme-to-light');
-        themeIcon.classList.add('_icon-theme-to-dark'); */
-
-      }
-    }
+  }
 
 
 })
 
 
-// =-=-=-=-=-=-=-=-=-=-=-=- <slider> -=-=-=-=-=-=-=-=-=-=-=-=
-/*
-let slider = new Swiper('.__slider', {
-  
-    spaceBetween: 30,
-    slidesPerView: 1,
-    centeredSlides: false,
+// =-=-=-=-=-=-=-=-=-=-=-=- <change header on scroll> -=-=-=-=-=-=-=-=-=-=-=-=
 
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-      992: {
-        slidesPerView: 3,
-        centeredSlides: true,
-    
-      },
-      600: {
-        slidesPerView: 2,
-        centeredSlides: false,
-      },
+function getCoords(elem) {
+  var box = elem.getBoundingClientRect();
+
+  return {
+    top: box.top + pageYOffset,
+    left: box.left + pageXOffset
+  };
+
+}
+
+function scrollPage() {
+
+  const offsetCheckJs = document.querySelector('.offset-check-js');
+  let top = [getCoords(offsetCheckJs).top, false];
+
+  header.classList.add('_loaded');
+
+  function scrollPageFunc() {
+    top[0] = getCoords(offsetCheckJs).top;
+
+    if (top[0] >= 300 && top[1] == false) {
+
+      top[1] = true;
+      header.style.setProperty('--pos', '-100%');
+
+      setTimeout(function () {
+        header.classList.add('_active');
+        header.style.setProperty('--pos', '0%');
+      }, 200);
+
+    } else if (top[0] <= 300 && top[1] == true) {
+
+      top[1] = false;
+      header.style.setProperty('--pos', '-100%');
+
+      setTimeout(function () {
+        header.style.setProperty('--pos', '0%');
+        header.classList.remove('_active');
+
+      }, 200);
+
     }
-}); 
-*/
-// =-=-=-=-=-=-=-=-=-=-=-=- </slider> -=-=-=-=-=-=-=-=-=-=-=-=
+  }
 
+  scrollPageFunc();
 
-/* 
-// =-=-=-=-=-=-=-=-=-=-=-=- <Анимации> -=-=-=-=-=-=-=-=-=-=-=-=
+  window.onscroll = scrollPageFunc;
 
-wow = new WOW({
-mobile:       false,
-})
-wow.init();
+}
 
-// =-=-=-=-=-=-=-=-=-=-=-=- </Анимации> -=-=-=-=-=-=-=-=-=-=-=-=
+scrollPage();
 
-*/
+// =-=-=-=-=-=-=-=-=-=-=-=- </change header on scroll> -=-=-=-=-=-=-=-=-=-=-=-=
+
